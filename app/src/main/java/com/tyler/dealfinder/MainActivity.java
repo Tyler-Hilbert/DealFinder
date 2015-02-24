d6c03c7ce5f847a0304479bd14f74f5e864e0e99
Brennan Hoeting <brennan.hoeting@gmail.com>
2015-02-23 19:31:20 -0500

    Add search

--- app/src/main/java/com/tyler/dealfinder/MainActivity.java
+++ app/src/main/java/com/tyler/dealfinder/MainActivity.java
@@ -3,45 +3,65 @@ package com.tyler.dealfinder;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-
 
 public class MainActivity extends Activity {
 	private ListView listingsListView;
+	private SearchView searchView;
+	private TextView statusView;
+	private ResultsRetrievalTaskRunner resultsRetriever;
+	private String query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.main);
 
 		listingsListView = (ListView) findViewById(R.id.listingsListView);
-		AsyncTaskRunner asyncTaskRunner = new AsyncTaskRunner();
-		asyncTaskRunner.execute();
+		statusView = (TextView) findViewById(R.id.status_text);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+		super.onCreateOptionsMenu(menu);
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.searchview_in_menu, menu);
+
+		MenuItem searchItem = menu.findItem(R.id.action_search);
+		searchView = (SearchView) searchItem.getActionView();
+		searchView.setIconifiedByDefault(true);
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			@Override
+			public boolean onQueryTextSubmit(String s) {
+				query = s;
+				resultsRetriever = new ResultsRetrievalTaskRunner();
+				resultsRetriever.execute();
+				searchView.clearFocus();
+				return false;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				return false;
+			}
+		});
+
         return true;
     }
 
@@ -60,13 +80,13 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected class AsyncTaskRunner extends AsyncTask<String, Void, ArrayList<EbayListing>> {
+	protected class ResultsRetrievalTaskRunner extends AsyncTask<String, Void, ArrayList<EbayListing>> {
         private ArrayList<EbayListing> resp;
 		ProgressDialog progressDialog;
 
 		@Override
 		protected void onPreExecute() {
-			progressDialog = ProgressDialog.show(MainActivity.this, "Loading products", "Loading products", true);
+			progressDialog = ProgressDialog.show(MainActivity.this, "Importing great deals", "Loading...", true);
 		}
 
 		@Override
@@ -74,7 +94,7 @@ public class MainActivity extends Activity {
             EbaySearch search = new EbaySearch();
 
             try {
-                resp = search.run("esp guitar");
+                resp = search.run(query);
             } catch(Exception e) {
                 e.printStackTrace();
             }
