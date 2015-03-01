8dd25d6a7cacce1216121133a722e2f743c742af
Brennan Hoeting <brennan.hoeting@gmail.com>
2015-03-01 12:34:02 -0500

    Add image memory caching

--- app/src/main/java/com/tyler/dealfinder/ListingArrayAdapter.java
+++ app/src/main/java/com/tyler/dealfinder/ListingArrayAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +18,22 @@ import java.util.ArrayList;
 public class ListingArrayAdapter extends ArrayAdapter<EbayListing> {
 
 	private final Context context;
-
+	private LruCache<String, Bitmap> mMemoryCache;
 	private final ArrayList<EbayListing> listingArrayList;
 
 	public ListingArrayAdapter(Context context, ArrayList<EbayListing> listingArrayList) {
 		super(context, R.layout.list_item, listingArrayList);
+		final int cacheSize = (int) (Runtime.getRuntime().maxMemory() / 1024) / 8;
+
 		this.context = context;
 		this.listingArrayList = listingArrayList;
+
+		this.mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+			@Override
+			protected int sizeOf(String key, Bitmap bitmap) {
+				return bitmap.getByteCount() / 1024;
+			}
+		};
 	}
 
 	public ArrayList<EbayListing> getValues() {
@@ -49,28 +59,58 @@ public class ListingArrayAdapter extends ArrayAdapter<EbayListing> {
 		priceView.setText("$" + listingArrayList.get(position).getPrice());
 		nameView.setText(listingArrayList.get(position).getName());
 		image.setContentDescription(listingArrayList.get(position).getName());
-        new DownloadImageTask(image).execute(listingArrayList.get(position).getPicUrl());
+		loadBitmap(image.getId() + position, image, listingArrayList.get(position).getPicUrl());
+
 
 		return rowView;
 	}
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+	public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+		if (getBitMapFromMemCache(key) == null) {
+			mMemoryCache.put(key, bitmap);
+		}
+	}
+
+	public Bitmap getBitMapFromMemCache(String key) {
+		return mMemoryCache.get(key);
+	}
+
+	public void loadBitmap(int resId, ImageView imageView, String bitmapUrl) {
+		final String imageKey = String.valueOf(resId);
+
+		final Bitmap bitmap = getBitMapFromMemCache(imageKey);
+		if (bitmap != null) {
+			imageView.setImageBitmap(bitmap);
+		} else {
+			BitmapWorkerTask task = new BitmapWorkerTask(imageView, resId);
+			task.execute(bitmapUrl);
+		}
+	}
+
+    private class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
-        public DownloadImageTask(ImageView bmImage) {
+		int resId;
+
+		public BitmapWorkerTask(ImageView bmImage, int resId) {
             this.bmImage = bmImage;
+			this.resId = resId;
         }
+
         protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
+            String url = urls[0];
             Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
+
+			try {
+                InputStream in = new java.net.URL(url).openStream();
                 mIcon11 = BitmapFactory.decodeStream(in);
+				addBitmapToMemoryCache(Integer.toString(resId), mIcon11);
             } catch (Exception e) {
-                //Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
+
             return mIcon11;
         }
+
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
         }
